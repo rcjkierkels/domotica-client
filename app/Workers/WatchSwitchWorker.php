2d00b7df0f74f266fcced91ec01b7ae6edbef7f4
Roland Kierkels <roland.kierkels@noveesoft.com>
2018-07-21 13:02:49 +0200

    Some last fixes

--- app/Workers/WatchSwitchWorker.php
+++ app/Workers/WatchSwitchWorker.php
@@ -22,22 +22,23 @@ class WatchSwitchWorker extends BaseWorker
 
     public function run()
     {
-        $value = $this->gpioService->read(3);
+        $inputPin = config('gpio.pins.input')[0];
+        $value = $this->gpioService->read($inputPin);
 
         if ($value !== true && $value !== false) {
             return;
         }
 
-        if (empty($this->data->lastSwitchStatus) || $this->data->lastSwitchStatus !== $value) {
+        if (!isset($this->data->lastSwitchStatus) || $this->data->lastSwitchStatus !== $value) {
             if ($value === false) {
                 Log::info('SWITCH', 'closed', 'Switch is closed');
             } else {
                 Log::info('SWITCH', 'open', 'Switch is open');
             }
 
-            $this->eventRepository->triggerEvent('SWITCH', ['status' => (int) !$value]);
-
             $this->data->lastSwitchStatus = $value;
+
+            $this->eventRepository->triggerEvent('SWITCH', ['status' => (int) $this->data->lastSwitchStatus]);
 
             $this->persistData();
         }
