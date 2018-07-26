92d78a7d37f30cfeeef3f16ec7a32e0e0ab890ae
Roland Kierkels <roland.kierkels@noveesoft.com>
2018-07-26 13:33:18 +0200

    New client update script

--- app/Console/Commands/UpdateClient.php
+++ app/Console/Commands/UpdateClient.php
@@ -2,10 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Log;
+use App\Repositories\ClientRepository;
 use Illuminate\Console\Command;
 
 class UpdateClient extends Command
 {
+    protected $clientRepository;
+
     /**
      * The name and signature of the console command.
      *
@@ -25,8 +29,9 @@ class UpdateClient extends Command
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(ClientRepository $clientRepository)
     {
+        $this->clientRepository = $clientRepository;
         parent::__construct();
     }
 
@@ -39,8 +44,48 @@ class UpdateClient extends Command
     {
         exec('bash '.app_path('../bin/git/status'), $result);
 
+        if (empty($result) || !isset($result[1])) {
+            Log::error('Update', 'Checking', 'Cannot get GIT status. Updating failed');
+            return;
+        }
+
+        $this->clientRepository->insertOrUpdateClient([
+            'last_update_check' => date('Y-m-d H:i:s')
+        ]);
+
+        switch($result[1])
+        {
+            case 'up-to-date':
+                // nothing to do
+                break;
+            case 'need-to-pull':
+                $this->update();
+                break;
+            case 'diverged':
+            case 'need-to-push':
+            Log::error('Update', 'Checking', 'Impossible to update due to local changes or conflicts');
+                break;
+            default:
+                Log::error('Update', 'Checking', 'Unknown GIT status. Updating failed!');
+                return;
+
+        }
+
+    }
+
+    protected function update()
+    {
         exec('git pull origin master');
 
-        die(var_dump($result));
+        exec('cat .git/refs/heads/master', $commitHash);
+
+        $commitHash = current($commitHash);
+
+        $this->clientRepository->insertOrUpdateClient([
+            'last_commit' => $commitHash,
+            'last_update_code' => date('Y-m-d H:i:s')
+        ]);
+
+        exec('composer install');
     }
 }
